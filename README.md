7ab2b21951e4088fd34298f207c7c0cae7dbc7b9
Lin Hao Lee <lhlee888@hotmail.com>
2020-04-25 16:25:21 +0100

    ready for 0.0.1

--- README.md
+++ README.md
@@ -1,65 +1,23 @@
-# lilypond-snippets README
+# LilyPond AutoComplete (Commands & Keywords)
 
-This is the README for your extension "lilypond-snippets". After writing up a brief description, we recommend including the following sections.
+Provides code snippets for command & keyword auto-complete and IntelliSense for LilyPond files. 
 
-## Features
+![Usage Demonstration](./assets/usage.gif)
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Included in the [VSLilyPond](https://marketplace.visualstudio.com/items?itemName=lhl2617.vslilypond) extension.
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Snippets are auto-generated from [LilyPond documentation](http://lilypond.org/doc/v2.20/Documentation/notation/lilypond-command-index) for LilyPond 2.20.0.
 
 ## Requirements
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+- (Optional but recommended): [VSLilyPond](https://marketplace.visualstudio.com/items?itemName=lhl2617.vslilypond) - Provides advanced LilyPond language support
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Please submit issues in the [GitHub repository](https://github.com/lhl2617/VSLilyPond-snippets)
 
-## Release Notes
+## Contributing
 
-Users appreciate release notes as you update your extension.
+Please submit issues/PRs in the [GitHub repository](https://github.com/lhl2617/VSLilyPond-snippets)
 
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,102 @@
 /// command index URL where lilypond command index is hosted
-const url = `http://lilypond.org/doc/v2.18/Documentation/notation/lilypond-command-index`;
+const url = `http://lilypond.org/doc/v2.20/Documentation/notation/lilypond-command-index`;
 
+import * as puppeteer from 'puppeteer';
+import * as fs from 'fs';
+
+/// details on page parsed as object
+type CodeTokenElement = {
+    code: string,
+    description: string,
+    href: string
+};
+
+type SnippetElement = {
+    prefix: string[],
+    body: string[],
+    description: string
+};
+
+
+const getCodeTokens = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.on('console', msg => {
+        console.log(msg.text());
+    });
+    await page.goto(url);
+    const elems = await page.evaluate(() => {
+        /*
+<tr>
+<td></td>
+<td valign="top">
+<a href="expressive-marks-attached-to-notes#index-_005c_0021-1">
+  <code>\!</code>
+</a>
+</td>
+<td valign="top">
+<a href="expressive-marks-attached-to-notes#dynamics">Dynamics</a>
+</td>
+</tr>
+         */
+
+        const htmlDecode = (input: string) => {
+            var e = document.createElement('textarea');
+            e.innerHTML = input;
+            // handle case of empty input
+            return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+        }
+
+
+        const tableRows = Array.from(document.querySelectorAll("table.index-ky > tbody > tr"));
+
+        const tableRowsWithThreeTds = tableRows.filter((tr: HTMLTableRowElement) => {
+            const children = Array.from(tr.children);
+            const tds = children.filter((c) => c.nodeName === `TD`);
+            return tds.length === 3;
+        })
+
+        const codeTokenElems = tableRowsWithThreeTds.map((tr: HTMLTableRowElement) => {
+            const children: HTMLTableCellElement[] = <HTMLTableCellElement[]>Array.from(tr.children);
+            const codeElement: Element = children[1].firstElementChild.firstElementChild;
+            const descElement: HTMLLinkElement = <HTMLLinkElement>children[2].firstElementChild;
+            const res: CodeTokenElement = {
+                code: htmlDecode(codeElement.innerHTML),
+                description: htmlDecode(descElement.innerHTML),
+                href: descElement.href
+            };
+            return res;
+        });
+
+        return codeTokenElems;
+    });
+
+    await browser.close();
+    return elems;
+};
+
+
+const processSnippetBody = (code: string) => {
+    if (code.includes(`...`)) {
+        return code.replace(`...`, `$0`);
+    }
+    return code;
+}
+
+/// MAIN ENTRY
+(async () => {
+    const elems = await getCodeTokens();
+    const snippetElems: SnippetElement[] =
+        elems.map((e: CodeTokenElement) => {
+            const res: SnippetElement = {
+                prefix: [e.code],
+                body: [processSnippetBody(e.code)],
+                description: `${e.description} - ${e.href}`
+            }
+            return res;
+        });
+    const snippetObject = Object.assign({}, snippetElems);
+    const snippetObjectString = JSON.stringify(snippetObject);
+
+    fs.writeFileSync('snippets/snippets.json', snippetObjectString);
+})();
