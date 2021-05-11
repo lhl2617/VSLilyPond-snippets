346a5a471ffd5b454210aa70ff0dbed901f64b78
lhl2617 <lhlee888@hotmail.com>
2021-05-11 18:46:51 +0100

    Fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
 /// command index URL where lilypond command index is hosted
 const url = `http://lilypond.org/doc/v2.22/Documentation/notation/lilypond-command-index`
 
-import * as puppeteer from 'puppeteer'
-import * as fs from 'fs'
+import * as puppeteer from "puppeteer"
+import * as fs from "fs"
 
 /// details on page parsed as object
 type CodeTokenElement = {
-    code: string
-    description: string
-    href: string
+  code: string
+  description: string
+  href: string
 }
 
 type SnippetElement = {
-    prefix: string[]
-    body: string[]
-    description: string
+  prefix: string[]
+  body: string[]
+  description: string
 }
 
 const getCodeTokens = async () => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    page.on('console', (msg) => {
-        console.log(msg.text())
-    })
-    await page.goto(url)
-    const elems = await page.evaluate(() => {
-        /*
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  page.on("console", (msg) => {
+    console.log(msg.text())
+  })
+  await page.goto(url)
+  const elems = await page.evaluate(() => {
+    /*
 <tr>
 <td></td>
 <td valign="top">
@@ -39,68 +39,68 @@ const getCodeTokens = async () => {
 </tr>
          */
 
-        const htmlDecode = (input: string) => {
-            var e = document.createElement('textarea')
-            e.innerHTML = input
-            // handle case of empty input
-            return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue
+    const htmlDecode = (input: string) => {
+      const e = document.createElement("textarea")
+      e.innerHTML = input
+      // handle case of empty input
+      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue
+    }
+
+    const tableRows = Array.from(
+      document.querySelectorAll("table.index-ky > tbody > tr")
+    )
+
+    const tableRowsWithThreeTds = tableRows.filter(
+      (tr: HTMLTableRowElement) => {
+        const children = Array.from(tr.children)
+        const tds = children.filter((c) => c.nodeName === `TD`)
+        return tds.length === 3
+      }
+    )
+
+    const codeTokenElems = tableRowsWithThreeTds.map(
+      (tr: HTMLTableRowElement) => {
+        const children: HTMLTableCellElement[] = <HTMLTableCellElement[]>(
+          Array.from(tr.children)
+        )
+        const codeElement: Element =
+          children[1].firstElementChild.firstElementChild
+        const descElement: HTMLLinkElement = <HTMLLinkElement>(
+          children[2].firstElementChild
+        )
+        const res: CodeTokenElement = {
+          code: htmlDecode(codeElement.innerHTML),
+          description: htmlDecode(descElement.innerHTML),
+          href: descElement.href,
         }
+        return res
+      }
+    )
 
-        const tableRows = Array.from(
-            document.querySelectorAll('table.index-ky > tbody > tr')
-        )
+    return codeTokenElems
+  })
 
-        const tableRowsWithThreeTds = tableRows.filter(
-            (tr: HTMLTableRowElement) => {
-                const children = Array.from(tr.children)
-                const tds = children.filter((c) => c.nodeName === `TD`)
-                return tds.length === 3
-            }
-        )
-
-        const codeTokenElems = tableRowsWithThreeTds.map(
-            (tr: HTMLTableRowElement) => {
-                const children: HTMLTableCellElement[] = <
-                    HTMLTableCellElement[]
-                >Array.from(tr.children)
-                const codeElement: Element =
-                    children[1].firstElementChild.firstElementChild
-                const descElement: HTMLLinkElement = <HTMLLinkElement>(
-                    children[2].firstElementChild
-                )
-                const res: CodeTokenElement = {
-                    code: htmlDecode(codeElement.innerHTML),
-                    description: htmlDecode(descElement.innerHTML),
-                    href: descElement.href,
-                }
-                return res
-            }
-        )
-
-        return codeTokenElems
-    })
-
-    await browser.close()
-    return elems
+  await browser.close()
+  return elems
 }
 
 const processSnippetBody = (code: string) => {
-    return code.replace(`...`, `$0`).replace(`…`, `$0`)
+  return code.replace(`...`, `$0`).replace(`…`, `$0`)
 }
 
 /// MAIN ENTRY
 ;(async () => {
-    const elems = await getCodeTokens()
-    const snippetElems: SnippetElement[] = elems.map((e: CodeTokenElement) => {
-        const res: SnippetElement = {
-            prefix: [e.code],
-            body: [processSnippetBody(e.code)],
-            description: `${e.description} - ${e.href}`,
-        }
-        return res
-    })
-    const snippetObject = Object.assign({}, snippetElems)
-    const snippetObjectString = JSON.stringify(snippetObject, null, `\t`)
+  const elems = await getCodeTokens()
+  const snippetElems: SnippetElement[] = elems.map((e: CodeTokenElement) => {
+    const res: SnippetElement = {
+      prefix: [e.code],
+      body: [processSnippetBody(e.code)],
+      description: `${e.description} - ${e.href}`,
+    }
+    return res
+  })
+  const snippetObject = Object.assign({}, snippetElems)
+  const snippetObjectString = JSON.stringify(snippetObject, null, `\t`)
 
-    fs.writeFileSync('snippets/snippets.json', snippetObjectString)
+  fs.writeFileSync("snippets/snippets.json", snippetObjectString)
 })()
